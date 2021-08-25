db94db3748386445aa8c849788f5318cae87d103
Aditya Andre <adityaandre33@gmail.com>
2021-08-25 14:46:20 +0700

    feat: wishlish

--- lib/widgets/buku.dart
+++ lib/widgets/buku.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:bossunapp/models/data_wishlist.dart';
 
-class Buku extends StatelessWidget {
+class Buku extends StatefulWidget {
   final detail;
   final username;
   Buku({required this.detail, this.username});
+
+  @override
+  _BukuState createState() => _BukuState(username, detail);
+}
+
+class _BukuState extends State<Buku> {
+  _BukuState(username, detail) {
+    var isFound = false;
+    dataWishlist.forEach((wish) {
+      if (wish.username == username && wish.idBuku == detail.id) isFound = true;
+    });
+    if (!isFound) {
+      isWishlist = false;
+    }
+  }
+
+  bool isWishlist = true;
+  void doWishlist(username, idBuku) {
+    dataWishlist.add(Wishlist(username: username, idBuku: idBuku));
+    dataWishlist.forEach((wish) {
+      print('=================');
+      print(wish.username);
+      print(wish.idBuku);
+      print('=================');
+    });
+    isWishlist = true;
+  }
+
+  void doRemoveWishlist(username, idBuku) {
+    dataWishlist.removeWhere(
+        (wish) => wish.username == username && wish.idBuku == idBuku);
+    // dataWishlist.remove(Wishlist(username: username, idBuku: idBuku));
+    dataWishlist.forEach((wish) {
+      print('=================');
+      print(wish.username);
+      print(wish.idBuku);
+      print('=================');
+    });
+    isWishlist = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +53,6 @@ class Buku extends StatelessWidget {
     // print(username);
     // print('================================================================');
 
-    void doWishlist(username, idBuku) {
-      dataWishlist.add(Wishlist(username: username, idBuku: idBuku));
-      dataWishlist.forEach((wish) {
-        print('=================');
-        print(wish.username);
-        print(wish.idBuku);
-        print('=================');
-      });
-    }
-
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -30,7 +60,7 @@ class Buku extends StatelessWidget {
         Container(
             width: 125,
             padding: EdgeInsets.all(10),
-            child: Image.asset(detail.imageAsset)),
+            child: Image.asset(widget.detail.imageAsset)),
         Expanded(
             child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -38,7 +68,7 @@ class Buku extends StatelessWidget {
             Container(
               padding: EdgeInsets.only(top: 10, right: 10),
               child: Text(
-                detail.judul,
+                widget.detail.judul,
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
             ),
@@ -47,7 +77,7 @@ class Buku extends StatelessWidget {
                 Container(
                   padding: EdgeInsets.only(top: 10),
                   child: Text(
-                    detail.author,
+                    widget.detail.author,
                     style: TextStyle(
                         fontSize: 12,
                         color: Colors.blue,
@@ -57,7 +87,7 @@ class Buku extends StatelessWidget {
                 Container(
                   padding: EdgeInsets.only(top: 10, left: 5),
                   child: Text(
-                    detail.tanggal,
+                    widget.detail.tanggal,
                     style: TextStyle(
                       fontSize: 11,
                     ),
@@ -69,7 +99,7 @@ class Buku extends StatelessWidget {
               children: [
                 Container(
                   child: Text(
-                    detail.publisher,
+                    widget.detail.publisher,
                     style: TextStyle(fontSize: 12),
                   ),
                 ),
@@ -79,33 +109,70 @@ class Buku extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                    margin: EdgeInsets.only(right: 10),
-                    child: Row(
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              doWishlist(username, detail.id);
-                              showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) =>
-                                      AlertDialog(
-                                        title: Text('Berhasil Wishlist'),
-                                        actions: [
-                                          TextButton(
-                                              onPressed: () {
-                                                Navigator.pop(context, 'OK');
-                                              },
-                                              child: Text('OK'))
-                                        ],
-                                      ));
-                            },
-                            child: Text(
-                              '+ Add to Wishlist',
-                              style: TextStyle(fontSize: 10),
-                            ))
-                      ],
-                    ))
+                isWishlist == true
+                    ? Container(
+                        margin: EdgeInsets.only(right: 10),
+                        child: Row(
+                          children: [
+                            TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    doRemoveWishlist(
+                                        widget.username, widget.detail.id);
+                                  });
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) =>
+                                          AlertDialog(
+                                            title: Text(
+                                                'Berhasil Remove Wishlist'),
+                                            actions: [
+                                              TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(
+                                                        context, 'OK');
+                                                  },
+                                                  child: Text('OK'))
+                                            ],
+                                          ));
+                                },
+                                child: Text(
+                                  'remove wishlist',
+                                  style: TextStyle(fontSize: 10),
+                                ))
+                          ],
+                        ))
+                    : Container(
+                        margin: EdgeInsets.only(right: 10),
+                        child: Row(
+                          children: [
+                            TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    doWishlist(
+                                        widget.username, widget.detail.id);
+                                  });
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) =>
+                                          AlertDialog(
+                                            title: Text('Berhasil Wishlist'),
+                                            actions: [
+                                              TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(
+                                                        context, 'OK');
+                                                  },
+                                                  child: Text('OK'))
+                                            ],
+                                          ));
+                                },
+                                child: Text(
+                                  '+ Add to Wishlist',
+                                  style: TextStyle(fontSize: 10),
+                                ))
+                          ],
+                        ))
               ],
             ),
             Row(
